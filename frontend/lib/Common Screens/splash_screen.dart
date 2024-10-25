e1679d4c0baa1416a14b2da944dc866daf5ad49f
Muhammad Zeeshan Akram <zeeshansiddiqui9790@gmail.com>
2024-10-25 22:46:20 +0500

    the assets deteled that are not useful

--- frontend/lib/Common Screens/splash_screen.dart
+++ frontend/lib/Common Screens/splash_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:frontend/Users/Modules/AUTHENTICATION/screens/login_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 import 'dart:ui';
 
 class SplashScreen extends StatefulWidget {
