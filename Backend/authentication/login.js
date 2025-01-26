c9379cf1fde7036c10b535617c49c9821d15f590
Muhammad Zeeshan Akram <zeeshansiddiqui9790@gmail.com>
2025-01-26 16:24:48 +0500

    9

--- Backend/authentication/login.js
+++ Backend/authentication/login.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = process.env; // Load JWT_SECRET from .env
 const User = require('../models/User');
 
-require('dotenv').config(); // Load .env variables
-const JWT_SECRET = process.env.JWT_SECRET; // Load JWT_SECRET
-
-// Login Route
 router.post('/login', async (req, res) => {
   const { loginType, password } = req.body;
 
@@ -28,14 +24,10 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ message: 'Invalid email/name or password' });
     }
 
-    const token = jwt.sign(
-      { id: user._id, email: user.email, name: user.name },
-      JWT_SECRET // Use JWT_SECRET from .env
-    );
-
+    // Respond with the static token from .env
     res.status(200).json({
       message: 'Login successful',
-      token,
+      token: JWT_SECRET, // Use the static token from .env
       user: { email: user.email, name: user.name },
     });
   } catch (error) {
